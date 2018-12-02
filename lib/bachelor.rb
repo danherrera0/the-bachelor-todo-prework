51cba0390cfd483ec079f34c927aad14fcc9be65
Dania Herrera <danherrera0@gmail.com>
2018-12-02 05:56:44 +0000

    Done.

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,46 +1,76 @@
+require "pry"
+
 def get_first_name_of_season_winner(data, season)
-  # code here
-  data.each do |season|
-    season.each do |name, age, hometown, occupation, status|
-      if status == "Winner"
-       name.split
-       puts name[0]
-     end
+  data.each do |season_num, participant_info|
+      if season_num == season
+        participant_info.each do |key|
+        value = key.values
+            if value.include?("Winner")
+            name = value[0].split
+            first_name=name[0]
+            return first_name
+            end
+        end
+       end
+    end
 end
 
 def get_contestant_name(data, occupation)
-  # code here
-  data.each do |season|
-    season.each do |name, age, hometown, job, status|
-    if job == occupation
-      puts name
-  end
-end
 
+  data.each do |season, participant_info|
+    participant_info.each do |key|
+    value = key.values
+    job = value[3]
+      if value.include?(occupation)
+      return value[0]
+#   data.each do |season|
+#     season.each do |name, age, hometown, job, status|
+#       if job == occupation
+#       puts name
+end
+end
+end
+end
+#
 def count_contestants_by_hometown(data, hometown)
-  # code here
-
-  data.each do |season|
-    season.each do |name, age, hometown, job, status|
-    hometown.collect
+counter = 0
+  data.each do |season, participant_info|
+    participant_info.each do |key|
+    value = key.values
+      if value.include?(hometown)
+      counter+=1
+      end
+    end
   end
+return counter
 end
-
+#
 def get_occupation(data, hometown)
-  # code here
-  data.each do |season|
-    season.each do |name, age, home, job, status|
-      if home == hometown
-        puts name
+  names =[]
+  data.each do |season, participant_info|
+    participant_info.each do |key|
+    value = key.values
+      if value.include?(hometown)
+            # binding.pry
+        names << value[3]
+    end
   end
+  end
+return names[0]
 end
 
+#
 def get_average_age_for_season(data, season)
-  data.each do |season|
-    season.each do |name, age, hometown, job, status|
-  # code here
-  age = age.to_i
-  average_age += age
+  average_age=0
+  age=[]
+  data.each do |season_num, participant_info|
+    if season_num == season
+        participant_info.collect do |key|
+        value = key.values
+        age << value[1].to_i
+        end
 end
-  puts average_age.round
+binding.pry
+end
+return average_age.to_f
 end
